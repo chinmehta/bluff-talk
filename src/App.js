182153e1dddbd6d011314ef3bf25f4902e02be4a
Chintan Mehta <mchintan24@gmail.com>
2022-12-23 00:00:21 +0000

    :heavy_plus_sign: dependencies added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Bluff Talk
+      <h1 className="text-3xl font-bold underline">
+      BluffTalk
+    </h1>
     </div>
   );
 }
